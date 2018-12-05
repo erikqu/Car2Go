0bcee019157d4935a20ccde09bf3801d0eb668a9
Miles H <mhood1119@gmail.com>
2018-12-04 23:44:27 -0600

    Fixed make table scripts for python and SQL

--- make_all_the_tables.py
+++ make_all_the_tables.py
@@ -22,159 +22,188 @@ print ("Any numbers that come up are error codes.")
 cleantables = 1
 if cleantables:
     command = '''
-                drop table "Model_parts";
-                drop table "Plants";
-                drop table "Options";
-                drop table "Customers";
-                drop table "Models";
-                drop table "Suppliers";
-                drop table "Supply";
-                drop table "Plant_supply";
-                drop table "Orders" ;
-                drop table "Parts";
-                drop table "Brands";
-                drop table "Dealers";
-                drop table "Vehicles";
+                drop table Orders;
+                drop table Supply;
+                drop table Plant_supply;
+                drop table Vehicles;
+                drop table Model_parts;
+                drop table Models;
+                drop table Parts;
+                drop table Brands;
+                drop table Dealers;
+                drop table Options;
+                drop table Plants;
+                drop table Customers;
+                drop table Suppliers;
 
                 '''
     cursor.execute(command)
     conn.commit()
 
 try:
-    command = '''CREATE TABLE "Model_parts" (
-                  "partId" char(4),
-                  "mid" char(4)
-                );
+    command = '''CREATE TABLE Options (
+		  oid char(4) NOT NULL,
+		  color varchar(10),
+		  engine varchar(10),
+		  transmission varchar(10),
+		  PRIMARY KEY (oid)
+		);
                 '''
     cursor.execute(command)
     conn.commit()
 except:
-    "Model_parts failed! This is the first table.  Clean tables?"
+    "Options failed! This is the first table.  Clean tables?"
 
 try:
-    command = '''CREATE TABLE "Plants" (
-                  "pid" char(4) NOT NULL,
-                  "model_name" varchar(20),
-                  PRIMARY KEY ("pid")
-                ); '''
+    command = '''CREATE TABLE Customers (
+		  cid char(4) NOT NULL,
+		  name varchar(30),
+		  address varchar(30),
+		  phone char(10),
+		  gender char(1),
+		  income numeric(10,2),
+		  PRIMARY KEY (cid)
+		); '''
     cursor.execute(command)
 except:
-    print ("Plants failed!")
+    print ("Customers failed!")
 
 try:
     command =    '''
-            CREATE TABLE "Options" (
-              "oid" char(4) NOT NULL,
-              "color" varchar(10),
-              "engine" varchar(10),
-              "transmission" varchar(10),
-              PRIMARY KEY ("oid")
-            ); '''
+            CREATE TABLE Brands (
+		  bid char(4) NOT NULL,
+		  brand_name varchar(20),
+		  PRIMARY KEY (bid)
+		); '''
     cursor.execute(command)
 except:
-    print("Options failed!")
+    print("Brands failed!")
 
 try:
 
     command=    '''
-                CREATE TABLE "Customers" (
-                  "cid" char(4) NOT NULL,
-                  "name" varchar(30),
-                  "address" varchar(30),
-                  "phone" char(10),
-                  "gender" char(1),
-                  "income" numeric(10,2),
-                  PRIMARY KEY ("cid")
-                );
+                CREATE TABLE Models (
+		  mid char(4) NOT NULL,
+		  model_name varchar(20),
+		  brand_name varchar(20),
+		  style varchar(10),
+		  bid char(4) NOT NULL,
+		  PRIMARY KEY (mid),
+		  FOREIGN KEY (bid) REFERENCES (Brands)
+		);
                 '''
     cursor.execute(command)
 except:
-    print("Customers failed!")
+    print("Models failed!")
 try:
     command=            '''
-                CREATE TABLE "Models" (
-                  "mid" char(4) NOT NULL,
-                  "model_name" varchar(20),
-                  "brand_name" varchar(20),
-                  "style" varchar(10),
-                  "bid" char(4) NOT NULL,
-                  PRIMARY KEY ("mid")
-                );
+                CREATE TABLE Suppliers (
+		  sid char(4) NOT NULL,
+		  model_name varchar(20),
+		  PRIMARY KEY (sid)
+		);
                 '''
     cursor.execute(command)
 except:
-    print ("Models failed!")
+    print ("Suppliers failed!")
 
 '''in reality either the first one fails or not.'''
 
 command=            '''
-            CREATE TABLE "Suppliers" (
-              "sid" char(4) NOT NULL,
-              "model_name" varchar(20),
-              PRIMARY KEY ("sid")
-            );'''
+            CREATE TABLE Plants (
+		  pid char(4) NOT NULL,
+		  model_name varchar(20),
+		  PRIMARY KEY (pid)
+		);'''
 cursor.execute(command)
 command=            '''
-            CREATE TABLE "Supply" (
-              "sid" char(4) NOT NULL,
-              "partId" char(4) NOT NULL
-            );'''
+	    CREATE TABLE Parts (
+		  partId char(4) NOT NULL,
+		  part_name varchar(20),
+		  PRIMARY KEY (partId)
+		);
+            '''
 cursor.execute(command)
 command=            '''
-            CREATE TABLE "Plant_supply" (
-              "pid" char(4) NOT NULL,
-              "partId" char(4) NOT NULL
-            );'''
+            CREATE TABLE Supply (
+			  sid char(4),
+			  partId char(4),
+			  PRIMARY KEY (sid, partId),
+			  FOREIGN KEY (sid) REFERENCES (Suppliers),
+			  FOREIGN KEY (partId) REFERENCES (Parts)
+			);
+'''
 
 cursor.execute(command)
-command=            '''CREATE TABLE "Orders" (
-              "did" char(4) NOT NULL,
-              "cid" char(4) NOT NULL,
-              "vin" varchar(17) NOT NULL
-            );'''
+command=            '''
+	    CREATE TABLE Plant_supply (
+			  pid char(4) NOT NULL,
+			  partId char(4) NOT NULL,
+			  PRIMARY KEY (pid, partId)
+			  FOREIGN KEY (pid) REFERENCES (Plants),
+			  FOREIGN KEY (partId) REFERENCES (Parts)
+			);
+'''
 cursor.execute(command)
-command=            '''CREATE TABLE "Parts" (
-              "partId" char(4) NOT NULL,
-              "part_name" varchar(20),
-              PRIMARY KEY ("partId")
-            );'''
+command=            '''
+            CREATE TABLE Dealers (
+			  did char(4) NOT NULL,
+			  dealer_name varchar(20),
+			  purchase_date date,
+			  brand_name varchar(10),
+			  model_name varchar(10),
+			  price numeric(12,2),
+			  color varchar(10),
+			  max_cars int,
+			  current_cars int,
+			  total_sold int,
+			  CHECK (current_cars <= max_cars)
+			  PRIMARY KEY (did)
+			);
+'''
 cursor.execute(command)
 
-command=            '''CREATE TABLE "Brands" (
-              "bid" char(4) NOT NULL,
-              "brand_name" varchar(20),
-              PRIMARY KEY ("bid")
-            );'''
+command=            '''
+	    CREATE TABLE Vehicles (
+			  vin varchar(17) NOT NULL,
+			  pid char(4) NOT NULL,
+			  oid char(4) NOT NULL,
+			  bid char(4) NOT NULL,
+			  mid char(4) NOT NULL,
+			  did char(4) NOT NULL,
+			  PRIMARY KEY (vin),
+			  FOREIGN KEY (pid) REFERENCES (Plants),
+			  FOREIGN KEY (oid) REFERENCES (Options),
+			  FOREIGN KEY (bid) REFERENCES (Brands),
+			  FOREIGN KEY (mid) REFERENCES (Models),
+			  FOREIGN KEY (did) REFERENCES (Dealers)
+			);
+'''
 cursor.execute(command)
-command=            '''CREATE TABLE "Dealers" (
-              "did" char(4) NOT NULL,
-              "dealer_name" varchar(20),
-              "date" date,
-              "brand_name" varchar(10),
-              "model_name" varchar(10),
-              "price" numeric(12,2),
-              "color" varchar(10),
-              "max_cars" int,
-              "current_cars" int,
-              "total_sold" int,
-              PRIMARY KEY ("did"),
-			  check (current_cars <= max_cars)
-            );'''
+command=            '''
+	    CREATE TABLE Orders (
+		  did char(4) NOT NULL,
+		  cid char(4) NOT NULL,
+		  vin varchar(17) NOT NULL,
+		  PRIMARY KEY (did, cid, vin),
+		  FOREIGN KEY (did) REFERENCES (Dealers),
+		  FOREIGN KEY (cid) REFERENCES (Customers),
+		  FOREIGN KEY (vin) REFERENCES (Vehicles)
+		);
+'''
 cursor.execute(command)
 try:
     command =       '''
-            CREATE TABLE "Vehicles" (
-              "vin" varchar(17) NOT NULL,
-              "pid" char(4),
-              "oid" char(4) NOT NULL,
-              "bid" char(4) NOT NULL,
-              "mid" char(4) NOT NULL,
-              "did" char(4),
-              PRIMARY KEY ("vin")
-            );'''
+            CREATE TABLE Model_parts (
+		  partId char(4),
+		  mid char(4),
+		  PRIMARY KEY (partId, mid),
+		  FOREIGN KEY (partId) REFERENCES (Parts),
+		  FOREIGN KEY (mid) REFERENCES (Models)
+		);'''
     cursor.execute(command)
 except:
-    print("Vehicles Failed!")
+    print("Model_parts Failed!")
 
 
 #this line commits the actual schema!
--- raw_create_tables.sql
+++ raw_create_tables.sql
@@ -1,107 +1,116 @@
-CREATE TABLE "Model_parts" (
-  "partId" char(4),
-  "mid" char(4)
+
+CREATE TABLE Options (
+  oid char(4) NOT NULL,
+  color varchar(10),
+  engine varchar(10),
+  transmission varchar(10),
+  PRIMARY KEY (oid)
 );
 
-CREATE INDEX "FK" ON  "Model_parts" ("partId", "mid");
-
-CREATE TABLE "Plants" (
-  "pid" char(4),
-  "model_name" varchar(20),
-  PRIMARY KEY ("pid")
+CREATE TABLE Customers (
+  cid char(4) NOT NULL,
+  name varchar(30),
+  address varchar(30),
+  phone char(10),
+  gender char(1),
+  income numeric(10,2),
+  PRIMARY KEY (cid)
 );
 
-CREATE TABLE "Options" (
-  "oid" char(4),
-  "color" varchar(10),
-  "engine" varchar(10),
-  "transmission" varchar(10),
-  PRIMARY KEY ("oid")
+CREATE TABLE Brands (
+  bid char(4) NOT NULL,
+  brand_name varchar(20),
+  PRIMARY KEY (bid)
 );
 
-CREATE TABLE "Customers" (
-  "cid" char(4),
-  "name" varchar(30),
-  "address" varchar(30),
-  "phone" char(10),
-  "gender" char(1),
-  "income" numeric(10,2),
-  PRIMARY KEY ("cid")
+CREATE TABLE Models (
+  mid char(4) NOT NULL,
+  model_name varchar(20),
+  brand_name varchar(20),
+  style varchar(10),
+  bid char(4) NOT NULL,
+  PRIMARY KEY (mid),
+  FOREIGN KEY (bid) REFERENCES (Brands)
 );
 
-CREATE TABLE "Models" (
-  "mid" char(4),
-  "model_name" varchar(20),
-  "brand_name" varchar(20),
-  "style" varchar(10),
-  "bid" char(4),
-  PRIMARY KEY ("mid")
+CREATE TABLE Suppliers (
+  sid char(4) NOT NULL,
+  model_name varchar(20),
+  PRIMARY KEY (sid)
 );
 
-CREATE INDEX "FK" ON  "Models" ("bid");
-
-CREATE TABLE "Suppliers" (
-  "sid" char(4),
-  "model_name" varchar(20),
-  PRIMARY KEY ("sid")
+CREATE TABLE Plants (
+  pid char(4) NOT NULL,
+  model_name varchar(20),
+  PRIMARY KEY (pid)
 );
 
-CREATE TABLE "Supply" (
-  "sid" char(4),
-  "partId" char(4)
+CREATE TABLE Parts (
+  partId char(4) NOT NULL,
+  part_name varchar(20),
+  PRIMARY KEY (partId)
 );
 
-CREATE INDEX "FK" ON  "Supply" ("sid", "partId");
-
-CREATE TABLE "Plant_supply" (
-  "pid" char(4),
-  "partId" char(4)
+CREATE TABLE Supply (
+  sid char(4),
+  partId char(4),
+  PRIMARY KEY (sid, partId),
+  FOREIGN KEY (sid) REFERENCES (Suppliers),
+  FOREIGN KEY (partId) REFERENCES (Parts)
 );
 
-CREATE INDEX "FK" ON  "Plant_supply" ("pid", "partId");
-
-CREATE TABLE "Orders" (
-  "did" char(4),
-  "cid" char(4),
-  "vin" varchar(17)
+CREATE TABLE Plant_supply (
+  pid char(4) NOT NULL,
+  partId char(4) NOT NULL,
+  PRIMARY KEY (pid, partId)
+  FOREIGN KEY (pid) REFERENCES (Plants),
+  FOREIGN KEY (partId) REFERENCES (Parts)
 );
 
-CREATE INDEX "FK" ON  "Orders" ("did", "cid", "vin");
-
-CREATE TABLE "Parts" (
-  "partId" char(4),
-  "part_name" varchar(20),
-  PRIMARY KEY ("partId")
+CREATE TABLE Dealers (
+  did char(4) NOT NULL,
+  dealer_name varchar(20),
+  purchase_date date,
+  brand_name varchar(10),
+  model_name varchar(10),
+  price numeric(12,2),
+  color varchar(10),
+  max_cars int,
+  current_cars int,
+  total_sold int,
+  CHECK (current_cars <= max_cars),
+  PRIMARY KEY (did)
 );
 
-CREATE TABLE "Brands" (
-  "bid" char(4),
-  "brand_name" varchar(20),
-  PRIMARY KEY ("bid")
+CREATE TABLE Vehicles (
+  vin varchar(17) NOT NULL,
+  pid char(4) NOT NULL,
+  oid char(4) NOT NULL,
+  bid char(4) NOT NULL,
+  mid char(4) NOT NULL,
+  did char(4) NOT NULL,
+  PRIMARY KEY (vin),
+  FOREIGN KEY (pid) REFERENCES (Plants),
+  FOREIGN KEY (oid) REFERENCES (Options),
+  FOREIGN KEY (bid) REFERENCES (Brands),
+  FOREIGN KEY (mid) REFERENCES (Models),
+  FOREIGN KEY (did) REFERENCES (Dealers)
 );
 
-CREATE TABLE "Dealers" (
-  "did" char(4),
-  "dealer_name" varchar(20),
-  "date" date,
-  "brand_name" varchar(10),
-  "model_name" varchar(10),
-  "price" numeric(12,2),
-  "color" varchar(10),
-  "max_cars" int,
-  "current_cars" int,
-  "total_sold" int,
-  PRIMARY KEY ("did")
+CREATE TABLE Orders (
+  did char(4) NOT NULL,
+  cid char(4) NOT NULL,
+  vin varchar(17) NOT NULL,
+  PRIMARY KEY (did, cid, vin),
+  FOREIGN KEY (did) REFERENCES (Dealers),
+  FOREIGN KEY (cid) REFERENCES (Customers),
+  FOREIGN KEY (vin) REFERENCES (Vehicles)
 );
 
-CREATE TABLE "Vehicles" (
-  "vin" varchar(17),
-  "pid" char(4),
-  "oid" char(4),
-  "bid" char(4),
-  "mid" char(4),
-  "did" char(4),
-  PRIMARY KEY ("vin")
+CREATE TABLE Model_parts (
+  partId char(4),
+  mid char(4),
+  PRIMARY KEY (partId, mid),
+  FOREIGN KEY (partId) REFERENCES (Parts),
+  FOREIGN KEY (mid) REFERENCES (Models)
 );
-
-CREATE INDEX "FK" ON  "Vehicles" ("pid", "oid", "bid", "mid", "did");
